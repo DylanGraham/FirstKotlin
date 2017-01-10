6f4c6c5441548e2b74f518dc51f65407ee175023
Dylan Graham <dylan.graham@gmail.com>
2017-01-10 15:35:43 +1100

    yay auto toString

--- src/First.kt
+++ src/First.kt
@@ -3,9 +3,9 @@ data class Person(var name: String,
                   var height: Float)
 
 fun main(args: Array<String>) {
-    val person: Person? = null
-    //val person: Person? = Person("Bob", 32, 1.7F)
-    //person?.name = "John"
+    //val person: Person? = null
+    val person: Person? = Person("Bob", 32, 1.7F)
+    person?.name = "John"
 
-    println(person?.name)
+    println(person)
 }
